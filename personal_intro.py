1b6ec4d7ea87937385714ed279f8a25f8385aea0
Thaufeeq Ahamed <thaufeeqahamednew@gmail.com>
2025-12-15 16:37:04 +0530

    Completed Week 1: Python Basics - Your First Steps in Programming

---
+++ personal_intro.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+hobby = input("What is your favorite hobby: ")
+
+print(f"Hello, my name is {name}. I am {age} years old and I love {hobby}.")
