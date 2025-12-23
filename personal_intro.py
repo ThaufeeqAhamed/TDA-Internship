a75019494b07052ed993b72efc9485046891725e
Thaufeeq Ahamed <thaufeeqahamednew@gmail.com>
2025-12-23 14:15:59 +0530

    Completed Week 2: Grade Calculator

--- personal_intro.py
+++
@@ -1,5 +0,0 @@
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-hobby = input("What is your favorite hobby: ")
-
-print(f"Hello, my name is {name}. I am {age} years old and I love {hobby}.")
